bfa5da2feb9801ecc22bf885d397efb170bdae00
DerekPeacock <derek.peacock@yahoo.com>
2020-09-13 17:29:11 +0100

    Added original BlueJ LabClass project

---
+++ lab-classes/Course.java
@@ -0,0 +1,33 @@
+
+/**
+ * This class stores information about a course
+ * that enrolled students may want to complete
+ *
+ * @author Derek Peacock
+ * @version 0.1 11/Sep/2020
+ */
+public class Course
+{
+    // instance variables - replace the example below with your own
+    private String codeNo;
+    private String title;
+
+    /**
+     * Constructor for objects of class Course
+     */
+    public Course(String codeNo, String title)
+    {
+        // initialise instance variables
+        this.codeNo = codeNo;
+        this.title = title;
+    }
+
+    /**
+     * Prints out the details of a course
+     */
+    public void print()
+    {
+        // put your code here
+        System.out.println("Course " + codeNo + " - " + title);
+    }
+}
