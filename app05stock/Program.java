55a3c25f6553824cac9cfdc62735edb53d8119a2
DerekPeacock <derek.peacock@yahoo.com>
2020-09-25 16:59:26 +0100

    Started Stock app

---
+++ app05stock/Program.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class Program here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Program
+{
+    private static StockApp app;
+
+    /**
+     * This class creates and runs an instance of
+     * the StockApp class
+     */
+    public static void main()
+    {
+        app = new StockApp();
+        app.run();
+    }
+}
---
+++ app05stock/StockApp.java
@@ -0,0 +1,73 @@
+
+/**
+ * This app provides a user interface to the
+ * stock manager so that users can add, edit,
+ * print and remove stock products
+ *
+ * @author Student Name
+ * @version 0.1
+ */
+public class StockApp
+{
+    // Use to get user input
+    private InputReader input;
+    
+    /**
+     * Constructor for objects of class StockApp
+     */
+    public StockApp()
+    {
+        input = new InputReader();
+    }
+
+    /**
+     * 
+     */
+    public void run()
+    {
+        printHeading();
+        getMenuChoice();
+    }
+    
+    /**
+     * 
+     */
+    public void getMenuChoice()
+    {
+        boolean finished = false;
+        
+        while(!finished)
+        {
+            printHeading();
+            printMenuChoices();
+           
+            String choice = input.getInput();
+            finished = true;
+        }
+    }
+    
+   
+    /**
+     * Print out a menu of operation choices
+     */
+    private void printMenuChoices()
+    {
+        System.out.println();
+        System.out.println("    Add:        Add a new product");
+        System.out.println("    Remove:     Remove an old product");
+        System.out.println("    PrintAll:   Print all products");
+        System.out.println("    Quit:       Quit the program");
+        System.out.println();        
+    }
+    
+    /**
+     * Print the title of the program and the authors name
+     */
+    private void printHeading()
+    {
+        System.out.println("******************************");
+        System.out.println(" Stock Management Application ");
+        System.out.println("    App05: by Student Name");
+        System.out.println("******************************");
+    }
+}
