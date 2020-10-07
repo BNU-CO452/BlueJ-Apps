2fcbc198505c616f428c35a3a9e3520dd0e21a7b
Derek Peacock <derek.peacock@yahoo.com>
2020-10-07 15:57:09 +0100

    Testing complete

--- app01-lab-classes/Student.java
+++ app01-lab-classes/Student.java
@@ -19,7 +19,6 @@ public class Student
     // A BSc course has 120 credits, each module has 15 Credits
     private int credits;
 
-    private Course course;
     
     /**
      * Create a new student with a given name and ID number.
@@ -82,20 +81,13 @@ public class Student
         return name.substring(0,4) + id.substring(0,3);
     }
     
-    /**
-     *  Added a course instance to the student
-     */
-    public void addCourse(Course course)
-    {
-        this.course = course;
-    }
-    
+   
     /**
      * Print the student's name and ID number to the output terminal.
      */
     public void print()
     {
         System.out.println(name + ", student ID: " + id + ", credits: " + credits);
-        course.print();
+
     }
 }
