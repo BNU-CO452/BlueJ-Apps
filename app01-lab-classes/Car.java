be73eccb0b2a10c8a0915de58ed4b85abda19cf6
Derek Peacock <derek.peacock@yahoo.com>
2020-10-11 15:04:38 +0100

    Produced API Documentation

---
+++ app01-lab-classes/Car.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of class Car here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Car
+{
+    // Constants
+    
+    // Attributes
+    
+    // Methods
+    
+    /**
+     * Constructor for objects of class Car
+     */
+    public Car(String parameter)
+    {
+        // this.attribute = paramter;
+    }
+}
---
+++ app01-lab-classes/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Car"},{"p":"<Unnamed>","l":"LabClass"},{"p":"<Unnamed>","l":"MobilePhone"},{"p":"<Unnamed>","l":"Student"}]
