f99ccb1c4bfd89591f64c0744617ce6d77d675d2
Derek Peacock <derek.peacock@yahoo.com>
2020-10-21 20:02:30 +0100

    Reset to start state

--- app03-studentGrades/Course.java
+++ app03-studentGrades/Course.java
@@ -11,16 +11,7 @@ public class Course
     // instance variables - replace the example below with your own
     private String codeNo;
     private String title;
-
-    private Module module1;
-    private Module module2;
-    private Module module3;
-    private Module module4;
     
-    private int finalMark;
-        
-    private Grades finalGrade;
-        
     /**
      * Constructor for objects of class Course
      */
@@ -29,19 +20,9 @@ public class Course
         // initialise instance variables
         this.codeNo = codeNo;
         this.title = title;
-        
-        module1 = new Module("Programming Concepts", "CO452");
-        //todo add the others
     }
 
-    public void addMark(int mark, int moduleNo)
-    {
-        if(moduleNo == 1)
-        {
-            module1.awardMark(mark);
-        }
-    }
-    
+   
     /**
      * Prints out the details of a course
      */
@@ -51,13 +32,4 @@ public class Course
         System.out.println("Course " + codeNo + " - " + title);
     }
     
-    public Grades convertToGrade(int mark)
-    {
-        if((mark >= 0) && (mark < 40))
-        {
-            return Grades.F;
-        }
-        
-        return Grades.X;
-    }
 }
