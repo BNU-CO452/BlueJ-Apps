0826f7be51a97457e995b089dece4160e5d47e65
Derek Peacock <derek.peacock@yahoo.com>
2021-08-22 19:44:47 +0100

    Added app215-songs

---
+++ app215-songs/Song.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class Song here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Song
+{
+
+    /**
+     * Constructor for objects of class Song
+     */
+    public Song()
+    {
+    }
+
+}
